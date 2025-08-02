fd03e6a43bfe55dc2303686f5cc14fd1c90fb4de
feichuan <2730879271@qq.com>
2025-08-02 16:44:28 +0800

    🐞 fix(api模块): 修复了无法部署的问题
    部署失败是因为 register.js 文件里重复导出了两个同名的 onRequest 函数，这违反了 ECMAScript 模块规范，现已经修改了该问题。
    0.00.00.17

--- functions/api/register.js
+++ functions/api/register.js
@@ -1,30 +1,3 @@
-export async function onRequest(context) {
-  const formData = await context.request.formData();
-  const token = formData.get('cf-turnstile-response');
-  const SECRET_KEY = '0x4AAAAAABnkZPrAemeKew_EP1Iu7fMmLXk';
-
-  const verifyRes = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-    method: 'POST',
-    body: new URLSearchParams({
-      secret: SECRET_KEY,
-      response: token,
-      remoteip: context.request.headers.get('CF-Connecting-IP')
-    })
-  });
-  const verifyData = await verifyRes.json();
-
-  return new Response(verifyData.success ? 'TRUE' : 'FALSE');
-}
-
-
-
-
-
-
-
-
-
-
 import mysql from 'mysql2/promise';
 
 export async function onRequest(context) {
